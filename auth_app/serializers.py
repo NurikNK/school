2131aa5f074567d325f9fe0a6c202f89255ca480
NurikNK <Nur_09_08_00@mail.ru>
2021-12-01 10:19:34 +0600

    Setup auth_app, token

--- auth_app/serializers.py
+++ auth_app/serializers.py
@@ -1,16 +1,18 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers, validators
+from rest_framework.authtoken.models import Token
 from employees.models import Employee, Position
 
 
 class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     email = serializers.EmailField(
         required=True, validators=[validators.UniqueValidator(queryset=User.objects.all())]
     )
     username = serializers.CharField(
         required=True, validators=[validators.UniqueValidator(queryset=User.objects.all())]
     )
-    password = serializers.CharField(required=True, min_length=8)
+    password = serializers.CharField(required=True, min_length=8, write_only=True)
     confirm_password = serializers.CharField(write_only=True, required=True, min_length=8)
     date_of_birth = serializers.DateField(write_only=True)
     image = serializers.ImageField(write_only=True, required=False, allow_null=True)
@@ -39,4 +41,11 @@ class UserSerializer(serializers.Serializer):
             position=validated_data['position'],
             salary=validated_data['salary'],
         )
+        Token.objects.create(user=user)
         return user
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        token = Token.objects.filter(user_id=instance.id).first()
+        response['token'] = token.key
+        return response
